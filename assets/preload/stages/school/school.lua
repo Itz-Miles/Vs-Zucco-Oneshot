b392e6867085840e42f0cc60d416d800d862fd58
sewshy.lua <m.elsheikh2008@outlook.com>
2024-02-04 23:34:57 +0300

    e

--- assets/preload/stages/school/school.lua
+++ assets/preload/stages/school/school.lua
@@ -1,18 +1,18 @@
 function onCreate()
 	makeLuaSprite('back', 'schoolassets/back', -1000, 150);
 	scaleObject('back', 1.2, 1.2);
-	addLuaSprite('back', false);
+	addLuaSprite('back', true);
 	
 	
 	makeLuaSprite('front', 'schoolassets/front', -1000, 150);
 	scaleObject('front', 1.2, 1.2);
-	addLuaSprite('front', false);
+	addLuaSprite('front', true);
 	
 	
 	makeAnimatedLuaSprite('zucco', 'schoolassets/zucco', -200, 550)
 	luaSpriteAddAnimationByPrefix('zucco', 'idle', 'zucco', 24, true);
 	scaleObject('zucco', 1.2, 1.2);
-	addLuaSprite('zucco', false);
+	addLuaSprite('zucco', true);
 
 	makeLuaSprite('overlay', 'schoolassets/overlay', -1000, 150);
 	scaleObject('overlay', 1.2, 1.2);

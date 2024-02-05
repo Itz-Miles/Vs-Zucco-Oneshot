d06234fa5d51da023e55d74ef4c20abb8a8917bb
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2024-02-04 21:05:46 -0700

    rename the stage

--- assets/preload/stages/hallway.lua
+++ assets/preload/stages/hallway.lua
@@ -1,20 +1,20 @@
 function onCreate()
-	makeLuaSprite('back', 'schoolassets/back', -1000, 150);
+	makeLuaSprite('back', 'back', -1000, 150);
 	scaleObject('back', 1.2, 1.2);
 	addLuaSprite('back', true);
 	
 	
-	makeLuaSprite('front', 'schoolassets/front', -1000, 150);
+	makeLuaSprite('front', 'front', -1000, 150);
 	scaleObject('front', 1.2, 1.2);
 	addLuaSprite('front', true);
 	
 	
-	makeAnimatedLuaSprite('zucco', 'schoolassets/zucco', -200, 550)
+	makeAnimatedLuaSprite('zucco', 'zucco', -200, 550)
 	luaSpriteAddAnimationByPrefix('zucco', 'idle', 'zucco', 24, true);
 	scaleObject('zucco', 1.2, 1.2);
 	addLuaSprite('zucco', true);
 
-	makeLuaSprite('overlay', 'schoolassets/overlay', -1000, 150);
+	makeLuaSprite('overlay', 'overlay', -1000, 150);
 	scaleObject('overlay', 1.2, 1.2);
 	addLuaSprite('overlay', true);
 	
